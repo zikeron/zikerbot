5ab46f321ec557861a3cd0f5aa4dad375c9f3334
Alejandro Cortes <alejandro.cortes@ironbit.com.mx>
2018-12-26 16:21:10 -0600

    update

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.post('/webhook/', (req, res) => {
     const webhook_event = req.body.entry[0];
     if (webhook_event.messaging) {
         webhook_event.messaging.forEach(event => {
-            handleMessage(event);
+            handleMessage(event.sender.id, event);
             //handleEvent(event.sender.id, event)
         });
     }
